89fb44f6919262c6040d76ffacd015712f3b40b1
RatneshChouhan <60027002+RatneshChouhan@users.noreply.github.com>
2022-02-27 01:24:40 +0530

    First Commit - Tight coupling example

--- src/main/java/com/rc/LearnSpringFrameworkApplication/game/GameRunner.java
+++ src/main/java/com/rc/LearnSpringFrameworkApplication/game/GameRunner.java
@@ -3,10 +3,15 @@ package com.rc.LearnSpringFrameworkApplication.game;
 public class GameRunner {
 
     private final MarioGame game;
+//  private final ContraGame game;
 
     public GameRunner(MarioGame game) {
         this.game = game;
     }
+
+    /*public GameRunner(ContraGame game) {
+        this.game = game;
+    }*/
 
     public void runGame() {
         this.game.up();
